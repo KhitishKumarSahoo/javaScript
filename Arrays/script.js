c914cdc92b2aaa8939b6a7586efd32375ffb5567
KhitishKumarSahoo <123398124+KhitishKumarSahoo@users.noreply.github.com>
2025-03-14 21:21:19 +0530

    Add files via upload

---
+++ Arrays/script.js
@@ -0,0 +1,16 @@
+//const username = 'Khitish Sahoo'
+
+const fruitsCollection = ['Apple', 'Banana', 'Grapes', 'Dates']
+
+
+//if we want to add any other fruits in array...then we can Simply write-->
+fruitsCollection.push('Water Melon')
+fruitsCollection[4] = 'Water Melon'
+
+//if we want to remove any fruits in array from backward side...then we can simply write-->
+fruitsCollection.pop()
+
+
+// const newObject = {}
+// const newArray = []
+console.log(fruitsCollection);
