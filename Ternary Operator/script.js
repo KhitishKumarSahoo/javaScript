c94a05e520c3fd5cdd859d15f3464e898307ec70
KhitishKumarSahoo <123398124+KhitishKumarSahoo@users.noreply.github.com>
2025-03-15 10:18:02 +0530

    Add files via upload

---
+++ Ternary Operator/script.js
@@ -0,0 +1,17 @@
+const username = 5 > 2 ? 'Anurag' : 'Procodrr'
+console.log(username)
+
+
+const gender = 'F'
+// debugger
+const userMessage = `${gender.toLocaleLowerCase() === 'f' ? 'She' : 'He'} is a college student.`
+console.log(userMessage);
+
+
+const result = null ? 'Anurag' : '' ? '12' : 0
+console.log(result)
+
+
+const age = 26;
+const beverage = age >= 21 ? "Beer" : "Juice";
+console.log(beverage); 
