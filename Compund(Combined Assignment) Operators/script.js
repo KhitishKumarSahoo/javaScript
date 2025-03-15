3092ba488861f04595dafcf584531ad79ec66685
KhitishKumarSahoo <123398124+KhitishKumarSahoo@users.noreply.github.com>
2025-03-15 13:11:07 +0530

    Add files via upload

---
+++ Compund(Combined Assignment) Operators/script.js
@@ -0,0 +1,70 @@
+let num = 5
+
+// debugger
+num = num + 5
+num = num + 5
+num = num + 5
+num = num + 5
+
+//+= Additions Assignment Operator
+num += 5
+num += 5    
+num += 5
+num += 5
+console.log(num);
+
+num = num - 5
+num = num - 5
+num = num - 5
+num = num - 5
+// -= Subtraction Assignment Operator
+num -= 5
+num -= 5
+num -= 5
+num -= 5
+console.log(num);
+
+num = num / 2
+num = num / 2
+num = num / 2
+num = num / 2
+// /= Division Assignment Operator
+num /= 2
+num /= 2
+num /= 2
+num /= 2
+console.log(num);
+
+
+num = num * 2
+num = num * 2
+num = num * 2
+// *= Multiplication Assignment Operator
+num *= 2
+num *= 2
+num *= 2
+console.log(num);
+
+
+num = num % 2
+num = num % 2
+num = num % 2
+// %= Modulus Assignment Operator
+num %= 2
+num %= 2
+num %= 2
+console.log(num);
+
+
+
+// const newNum = ++num
+// const newNum = --num
+
+// const newNum = num++
+// const newNum = num--
+
+// num = num + 1
+// num += 1
+
+// num++
+
