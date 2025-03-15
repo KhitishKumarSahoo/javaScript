4a95c3985e7a66eaf3c6f5ba9ea5acf0c4054dc3
KhitishKumarSahoo <123398124+KhitishKumarSahoo@users.noreply.github.com>
2025-03-15 12:34:54 +0530

    Add files via upload

---
+++ indexOf() in Arrays/script.js
@@ -0,0 +1,10 @@
+const evenNumbers = [2, 4, 6, 8]
+const friends = ['sahoo', 'Abhi', 'Alok', 'manoj']
+
+//Bessically indexOf() method returns the first index at which a given element can be found in the array, 
+//-1 if it is not present.
+evenNumbers.indexOf(6)
+friends.indexOf('manoj')
+
+console.log(evenNumbers.indexOf(6));
+console.log(friends.indexOf('manoj'));
