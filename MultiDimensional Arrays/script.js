4f92d18b3f33a8d2ca9edffb68c2e34fae8338fb
KhitishKumarSahoo <123398124+KhitishKumarSahoo@users.noreply.github.com>
2025-03-15 12:35:29 +0530

    Add files via upload

---
+++ MultiDimensional Arrays/script.js
@@ -0,0 +1,11 @@
+
+const nameAndNumberList1 = ['Khitish', 'Kumar', 'Sahoo', 123, 456, 789];
+
+// console.log(nameAndNumberList1);
+// console.log(nameAndNumberList1[0],);
+
+//multidimensional array or nested array or array inside arrays 
+const nameAndNumberList2 = [['Khitish', 'Kumar'], ['Sahoo', 123], [456, 789]];
+console.log(nameAndNumberList2[0][1]);
+console.log(nameAndNumberList2[1][0]);
+console.log(nameAndNumberList2[2][1]);
